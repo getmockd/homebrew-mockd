5c63a21d560a13d1bbe7776dc76008d4b8e12317
Zach Snell <zach20snell10@gmail.com>
2026-02-15 23:42:42 -0600

    chore: update formula to v0.2.8 with correct SHA256 hashes

--- Formula/mockd.rb
+++ Formula/mockd.rb
@@ -1,26 +1,26 @@
 class Mockd < Formula
   desc "High-performance multi-protocol API mocking server"
   homepage "https://mockd.io"
-  version "0.2.2"
+  version "0.2.8"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/getmockd/mockd/releases/download/v#{version}/mockd-darwin-arm64"
-      sha256 "639cc7235e09bba74e89439b7a4a6a43eb635c3561a023b132d034e80678c336"
+      sha256 "0047237c2501edc65938a9603c7f39c45f4ea934b9990daa3831c14b5e9c4c22"
     else
       url "https://github.com/getmockd/mockd/releases/download/v#{version}/mockd-darwin-amd64"
-      sha256 "fa457050c5a4f5b7b27782fb5938a210c70ad735bc196bb0a78659d62bf13b55"
+      sha256 "cc38ffff5b51a32ed5688b496c74583f7afbe139ab3d4be1857846dc2b5eb09b"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/getmockd/mockd/releases/download/v#{version}/mockd-linux-arm64"
-      sha256 "06270672768c8fa3d32e106293213037c9fb8eca2d7f95c7fdd4a8c356ed24ad"
+      sha256 "735fccc85957be6f0c61b03367f4443e149cc6b43fedff0f5248e7a053552f6f"
     else
       url "https://github.com/getmockd/mockd/releases/download/v#{version}/mockd-linux-amd64"
-      sha256 "5fe10c03c1fb733f30cebf580db789798f3166e78def95972209e3660427b7d5"
+      sha256 "8d80fd1b26065dd21b9baa8197e94db261716e19add62606398b3ff3ff756658"
     end
   end
 
